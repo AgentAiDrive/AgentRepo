6167ef6dbf89905612a8dcd3f7e8d393a85d2430
AgentAiDrive <agentaidrive@gmail.com>
2025-08-01 01:58:20 -0700

    Update rag_utils.py

--- rag_utils.py
+++ rag_utils.py
@@ -1,4 +1,4 @@
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 
 VECTORSTORE_DIR = "chromastore"
@@ -9,9 +9,15 @@ def ingest_files(uploaded_files):
         text = f.read().decode("utf-8", errors="ignore")
         docs.append(text)
     embeds = OpenAIEmbeddings()
-    store = Chroma.from_texts(docs, embedding=embeds, persist_directory=VECTORSTORE_DIR)
-    store.persist()
+    store = Chroma.from_texts(
+        texts=docs,
+        embedding_function=embeds,
+        persist_directory=VECTORSTORE_DIR
+    )
     return store
 
 def load_retriever():
-    return Chroma(persist_directory=VECTORSTORE_DIR, embedding_function=OpenAIEmbeddings()).as_retriever()
+    return Chroma(
+        persist_directory=VECTORSTORE_DIR,
+        embedding_function=OpenAIEmbeddings()
+    ).as_retriever()
